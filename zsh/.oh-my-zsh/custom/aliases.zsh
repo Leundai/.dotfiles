649f4b6fe5edbba0a81d37a2bbf90a3b8530c935
leundai <leogalindofrias@gmail.com>
2025-05-13 21:21:28 -0400

    KO stuff and cleanup formatting

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -4,7 +4,4 @@ alias vim='nvim'
 export GOPATH="$HOME/go"
 export PATH="${PATH}:${GOPATH}/bin"
 
-export KO_DOCKER_REPO='docker.io/leundai0'
-export KO_DEFAULTPLATFORM='linux/arm64'
-
 . "$HOME/.cargo/env"
